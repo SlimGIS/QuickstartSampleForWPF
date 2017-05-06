58c0e0e13e8ce2346d41315a0dc9478a94766b4d
SlimGis_dev <dev@slimgis.com>
2017-05-06 23:56:35 +0800

    Add footer and test.

---
+++ QuickstartSampleForWPF/Footer.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QuickstartSampleForWPF
+{
+    /// <summary>
+    /// Interaction logic for Footer.xaml
+    /// </summary>
+    public partial class Footer : UserControl
+    {
+        public Footer()
+        {
+            InitializeComponent();
+        }
+
+        private void QuestionButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://forums.slimgis.com/");
+        }
+
+        private void ContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Application.Current.MainWindow, "Please send email to \"support@slimgis.com\".", "Contact us", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://www.slimgis.com/");
+        }
+
+        private void LearnMoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://blog.slimgis.com/");
+        }
+    }
+}
